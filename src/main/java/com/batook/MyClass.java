dfad5b57c2863306fa9014eddae1d19c3ec370e6
batook <batook@mail.ru>
2019-05-26 00:21:03 +0300

    init

---
+++ src/main/java/com/batook/MyClass.java
@@ -0,0 +1,17 @@
+package com.batook;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project MyProject
+ * Created by batook on 25.05.2019 22:24
+ */
+
+public class MyClass {
+    private static final Logger log = LoggerFactory.getLogger(MyClass.class);
+
+    public static void main(String[] args) {
+        log.info("Hello");
+    }
+}
